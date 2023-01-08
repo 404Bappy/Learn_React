3426efed1b73ae4edf947392ea52c3da0a4a64ce
Bappy71 <19103076@iubat.edu>
2023-01-09 04:51:55 +0600

    Initial commit

--- structure-react/src/index.js
+++ structure-react/src/index.js
@@ -6,5 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div>
     <h1>Todo App</h1>
+    <h4>create it</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet accusantium voluptatibus vitae minus sunt praesentium nobis cupiditate, quae, dolorum nam eligendi. Vitae cumque ut sapiente eum molestiae dicta officiis modi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non provident ut, atque vero laudantium saepe eveniet odit esse dolor aliquam nemo nesciunt mollitia amet numquam iste veritatis aliquid dolorum illum.</p>
+    <p>19/1/22</p>
+  </div>
+
 );
