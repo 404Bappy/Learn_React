eef2b1e6d2956e6e9f9fc05442ba2e65cb74f1b7
Bappy71 <19103076@iubat.edu>
2023-01-19 23:53:25 +0600

    Initial commit

--- Rest-Countries/index.js
+++ Rest-Countries/index.js
@@ -1,11 +1,24 @@
-const loadCountries =() =>{
+const loadCountries = () => {
     fetch('https://restcountries.com/v2/all')
-    .then(res => res.json() )
-    .then(data => displayCountries(data));
+        .then(res => res.json())
+        .then(data => displayCountries(data));
 }
 
 const displayCountries = countries => {
-    console.log(countries);
+    const CountryHtmls = countries.map(country => getCountry(country));
+console.log(CountryHtmls);
+}
+
+const getCountry = country => {
+    return `
+<div>
+<h2>${country.name}</h2>
+<h4>Capital : ${country.capital}</h4>
+<img src ="${country.flag}">
+
+</div>
+
+`
 }
 
 loadCountries();
