f5cc394836afbe3b30099b3ce3c10a4de6bda419
Bappy71 <19103076@iubat.edu>
2023-01-23 02:35:26 +0600

    Initial commit

--- React-Core-Concepts/react-things/src/App.js
+++ React-Core-Concepts/react-things/src/App.js
@@ -6,24 +6,38 @@ function App() {
 
   return (
     <div className="App">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person Name = "BAPPY" Job = "Footballer"></Person>
+        <Person Name = "BASHAR" Job = "Cricketer"></Person>
+        <Person Name = "KHAIRUL" Job = "Body-Builder"></Person>
+        <Person Name = "ARAFAT" Job = "Developer"></Person>
+       
+       <Car Name = "Audi" ></Car>
+
+        <Car></Car>
+
     </div>
   );
 }
 
-function Person(){
+function Person(props){
+  console.log(props);
   return(
      <div className="Person">
-        <h1>Name: Arfat Bappy</h1>
-        <h4>Profession: Footballer</h4>
-        <h6>Age : 26</h6>
+        <h1>Name: {props.Name}</h1>
+        <h4>Profession: {props.Job}</h4>
      </div>
   );
+}
+
+function Car(){
+  return(
+    <div className = "Person">
+      <h1>Car Name : Audi</h1>
+      <h3>Color : RED</h3>
+      <h5>Registration Year : 2019</h5>
+      <h6>Price : 98,00000</h6>
+    </div>
+  )
 }
 
 export default App;
