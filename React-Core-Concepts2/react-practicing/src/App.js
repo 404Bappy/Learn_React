6676cffd2f55963572e8c5fd601bc74438994523
Bappy71 <19103076@iubat.edu>
2023-01-24 05:02:03 +0600

    Initial commit

--- React-Core-Concepts2/react-practicing/src/App.js
+++ React-Core-Concepts2/react-practicing/src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <Product Name="iphone" ></Product>
     </div>
   );
+}
+
+
+function Product(props) {
+  return (
+    <div className ="Product">
+      <h2>Name : {props.name}</h2>
+      <h2>Price : {props.price}</h2>
+    </div>
+
+  )
+
 }
 
 export default App;
