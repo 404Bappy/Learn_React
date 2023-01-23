b4ec632fbc6b52b4140f2fb5a9477831b0c06ae6
Bappy71 <19103076@iubat.edu>
2023-01-24 05:14:41 +0600

    Initial commit

--- React-Core-Concepts2/react-practicing/src/App.js
+++ React-Core-Concepts2/react-practicing/src/App.js
@@ -2,9 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name : 'mobile', price : '500$'},
+    {name : 'Laptop', price : '1000$'},
+    {name : 'AirBud', price : '500$'},
+    {name : 'HUB', price : '500$'},
+    {name : 'SmartWatch', price : '500$'}
+  ]
+
+
   return (
     <div className="App">
-      <Product Name="iphone" ></Product>
+      
+      {
+        products.map(product => <Product name = {product.name}  price ={product.price}></Product>)
+      }
+      
+      
+      {/* <Product name="iphone-12" price = "600$" ></Product>
+      <Product name="iphone-12" price = "600$" ></Product>
+      <Product name="iphone-12" price = "600$" ></Product>
+      <Product name="iphone-12" price = "600$" ></Product>
+      <Product name="iphone-12" price = "600$" ></Product> */}
     </div>
   );
 }
