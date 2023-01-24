7a54fc8e3738f110818a48bd2586c5dc0b750ad1
Bappy71 <19103076@iubat.edu>
2023-01-24 23:49:04 +0600

    Initial commit

--- React-Core-Concepts2/react-practicing/src/App.js
+++ React-Core-Concepts2/react-practicing/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   const products = [
-    {name : 'mobile', price : '500$'},
-    {name : 'Laptop', price : '1000$'},
-    {name : 'AirBud', price : '500$'},
-    {name : 'HUB', price : '500$'},
-    {name : 'SmartWatch', price : '500$'},
-    {name : 'astst', price : '500$'}
+    {name : 'mobile', price : '500$' ,cashback : "26%"},
+    {name : 'Laptop', price : '1000$' ,cashback : "26%"},
+    {name : 'AirBud', price : '500$' ,cashback : "26%"},
+    {name : 'HUB', price : '500$', cashback : "26%"},
+    {name : 'SmartWatch', price : '500$' , cashback : "26%"},
+    {name : 'astst', price : '500$' , cashback : "26%"}
     
   ]
 
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       
       {
-        products.map(product => <Product name = {product.name}  price ={product.price}></Product>)
+        products.map(product => <Product name = {product.name}  price ={product.price} cashback = {product.cashback}></Product>)
       }
       
       
@@ -36,6 +36,7 @@ function Product(props) {
     <div className ="Product">
       <h2>Name : {props.name}</h2>
       <h2>Price : {props.price}</h2>
+      <h3>Cash Back : {props.cashback}</h3>
     </div>
 
   )
