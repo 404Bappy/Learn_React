311ec352c74ac684ab7897749cbefeb25c20f6dd
Bappy71 <19103076@iubat.edu>
2023-01-25 04:28:30 +0600

    Initial commit

--- React-Core-Concepts2/react-practicing/src/App.js
+++ React-Core-Concepts2/react-practicing/src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 
 function App() {
   const Crickters = [
-    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Taskin Ahmed", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Mehedi Miraz", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Mustafiz", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Nasir Hossain", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Shommoya Sarkar", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
-    {name : "Tamim Iqbal", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"}
+    { name: "Sakib Al Hasan", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Taskin Ahmed", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Mehedi Miraz", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Mustafiz", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Nasir Hossain", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Shommoya Sarkar", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" },
+    { name: "Tamim Iqbal", Nationality: "Bangladeshi", Bidding_Price: "10 lakh" }
   ]
-  
-  
-  
-  
-  
+
+
+
+
+
   // const products = [
   //   {name : 'mobile', price : '500$' ,cashback : "26%"},
   //   {name : 'Laptop', price : '1000$' ,cashback : "26%"},
@@ -23,16 +23,16 @@ function App() {
   //   {name : 'HUB', price : '500$', cashback : "26%"},
   //   {name : 'SmartWatch', price : '500$' , cashback : "26%"},
   //   {name : 'astst', price : '500$' , cashback : "26%"}
-    
+
   // ]
 
 
   return (
     <div className="App">
 
-    {
-      Crickters.map(cricketer => <Cricketer name = {cricketer.name} Nationality = {cricketer.Nationality} Bidding_Price = {cricketer.Bidding_Price} ></Cricketer>)
-    }
+      {
+        Crickters.map(cricketer => <Cricketer name={cricketer.name} Nationality={cricketer.Nationality} Bidding_Price={cricketer.Bidding_Price} ></Cricketer>)
+      }
 
 
 
@@ -40,8 +40,8 @@ function App() {
       {/* {
         products.map(product => <Product name = {product.name}  price ={product.price} cashback = {product.cashback}></Product>)
       } */}
-      
-      
+
+
       {/* <Product name="iphone-12" price = "600$" ></Product>
       <Product name="iphone-12" price = "600$" ></Product>
       <Product name="iphone-12" price = "600$" ></Product>
@@ -54,7 +54,7 @@ function App() {
 
 function Cricketer(props) {
   return (
-    <div className ="Product">
+    <div className="Product">
       <h2>Name : {props.name}</h2>
       <h2>Bidding_Price : {props.Bidding_Price}</h2>
       <h3>Nationality : {props.Nationality}</h3>
