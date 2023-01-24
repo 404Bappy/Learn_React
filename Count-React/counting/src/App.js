e3d26bad187a8136b84686a281b16224a50f4bd8
Bappy71 <19103076@iubat.edu>
2023-01-25 05:25:54 +0600

    Initial commit

--- Count-React/counting/src/App.js
+++ Count-React/counting/src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter></Counter>
     </div>
   );
+}
+
+function Counter(){
+  return(
+    <div className = "Counter">
+      <h1>Here is the Counter</h1>
+      <button className = "btn">Increse  +</button>
+      <br />
+      <br />
+      <button className = "btn1">Decrese -</button>
+
+    </div>
+  )
 }
 
 export default App;
