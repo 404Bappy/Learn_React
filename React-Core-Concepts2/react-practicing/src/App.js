65499319541b51f976777c42119faa9c5fe6bfa9
Bappy71 <19103076@iubat.edu>
2023-01-25 00:07:59 +0600

    Initial commit

--- React-Core-Concepts2/react-practicing/src/App.js
+++ React-Core-Concepts2/react-practicing/src/App.js
@@ -2,23 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const products = [
-    {name : 'mobile', price : '500$' ,cashback : "26%"},
-    {name : 'Laptop', price : '1000$' ,cashback : "26%"},
-    {name : 'AirBud', price : '500$' ,cashback : "26%"},
-    {name : 'HUB', price : '500$', cashback : "26%"},
-    {name : 'SmartWatch', price : '500$' , cashback : "26%"},
-    {name : 'astst', price : '500$' , cashback : "26%"}
-    
+  const Crickters = [
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"},
+    {name : "Sakib Al Hasan", Nationality : "Bangladeshi" , Bidding_Price : "10 lakh"}
   ]
+  
+  
+  
+  
+  
+  // const products = [
+  //   {name : 'mobile', price : '500$' ,cashback : "26%"},
+  //   {name : 'Laptop', price : '1000$' ,cashback : "26%"},
+  //   {name : 'AirBud', price : '500$' ,cashback : "26%"},
+  //   {name : 'HUB', price : '500$', cashback : "26%"},
+  //   {name : 'SmartWatch', price : '500$' , cashback : "26%"},
+  //   {name : 'astst', price : '500$' , cashback : "26%"}
+    
+  // ]
 
 
   return (
     <div className="App">
-      
-      {
+
+    {
+      Crickters.map(cricketer => <Cricketer name = {cricketer.name} Nationality = {cricketer.Nationality} Bidding_Price = {cricketer.Bidding_Price} ></Cricketer>)
+    }
+
+
+
+
+      {/* {
         products.map(product => <Product name = {product.name}  price ={product.price} cashback = {product.cashback}></Product>)
-      }
+      } */}
       
       
       {/* <Product name="iphone-12" price = "600$" ></Product>
@@ -31,12 +52,12 @@ function App() {
 }
 
 
-function Product(props) {
+function Cricketer(props) {
   return (
     <div className ="Product">
       <h2>Name : {props.name}</h2>
-      <h2>Price : {props.price}</h2>
-      <h3>Cash Back : {props.cashback}</h3>
+      <h2>Bidding_Price : {props.Bidding_Price}</h2>
+      <h3>Nationality : {props.Nationality}</h3>
     </div>
 
   )
