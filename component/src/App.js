49bb40f13319a697c5547d31ec187cef49e3e532
Bappy71 <19103076@iubat.edu>
2023-01-29 03:30:53 +0600

    first commit

--- component/src/App.js
+++ component/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h2>Hi there</h2>
+          <h2>This is Md. Khairul Bashar </h2>
+          <h2>id: 19103076</h2>
+<h5>im doing bcse at iubat for last 4 years</h5>
     </div>
   );
 }
