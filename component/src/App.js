f2e028095f1079ab94e8bb8f797b6e102d7bd629
Bappy71 <19103076@iubat.edu>
2023-02-07 03:45:22 +0600

    first commit

--- component/src/App.js
+++ component/src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hi there</h2>
-      <h2>This is Md. Khairul Bashar </h2>
-      <h2>id: 19103076</h2>
-      <h5>im doing bcse at iubat for last 4 years</h5>
+  
     </div>
   );
 }
