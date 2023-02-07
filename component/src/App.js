3a5b09a25f177c5e17e8fb2473381411df6ab6f0
Bappy71 <19103076@iubat.edu>
2023-02-08 02:30:04 +0600

    first commit

--- component/src/App.js
+++ component/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
+import { useState } from 'react';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+
   return (
     <div className="App">
-      <Device name="PHone"  Price="10k" />
+      <h3>My steps : {steps}</h3>
+      <button>Walk</button>
+      <Device name="PHone" Price="10k" />
     </div>
   );
 }
