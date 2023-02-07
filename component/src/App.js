d84a186236a0c6b6e73311e1b5eb516c031c0895
Bappy71 <19103076@iubat.edu>
2023-02-08 02:33:26 +0600

    first commit

--- component/src/App.js
+++ component/src/App.js
@@ -5,11 +5,15 @@ import { useState } from 'react';
 
 function App() {
   const [steps, setSteps] = useState(0);
+  const handleIncreaseSteps = () => {
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount);
+  }
 
   return (
     <div className="App">
       <h3>My steps : {steps}</h3>
-      <button>Walk</button>
+      <button onClick={handleIncreaseSteps}>Walk</button>
       <Device name="PHone" Price="10k" />
     </div>
   );
