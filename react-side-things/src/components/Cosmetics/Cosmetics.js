d8a4e31e8b69e0b1e1f31152acdd327c6ad3962d
Bappy71 <19103076@iubat.edu>
2023-02-13 00:48:54 +0600

    first commit

--- react-side-things/src/components/Cosmetics/Cosmetics.js
+++ react-side-things/src/components/Cosmetics/Cosmetics.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { add, multiply, substract } from '../../utilities/storage';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 89;
-    const sum = add(first, second);
-    const multi = multiply(first, second);
-    const sub = substract(first, second);
+    const cosmetics = [
+        { id: 1, name: 'Alta', price: 25 },
+        { id: 1, name: 'Nail Polish', price: 100 },
+        { id: 1, name: 'Face MAsk', price: 50 },
+        { id: 1, name: 'FaceWash', price: 500 }
+    ]
 
     return (
         <div>
-        
+            <h1> My Cosmetics Shop !</h1>
         </div>
     );
 };
 
 export default Cosmetics;
+
+
+
+// const first = 55;
+// const second = 89;
+// const sum = add(first, second);
+// const multi = multiply(first, second);
+// const sub = substract(first, second);
