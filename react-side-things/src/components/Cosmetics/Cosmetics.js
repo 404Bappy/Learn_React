8eca116a626a08b72e4980e27971a37a76f5d6ea
Bappy71 <19103076@iubat.edu>
2023-02-12 23:12:41 +0600

    first commit

--- react-side-things/src/components/Cosmetics/Cosmetics.js
+++ react-side-things/src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { add } from '../../utilities/storage';
 
 const Cosmetics = () => {
+    const first = 55;
+    const second = 89;
+    const sum = add(first, second);
+    
     return (
         <div>
-            
+
         </div>
     );
 };
