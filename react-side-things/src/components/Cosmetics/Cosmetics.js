93b7559220dc22791f8a32d89d305c63ab6a289f
Bappy71 <19103076@iubat.edu>
2023-02-12 23:20:32 +0600

    first commit

--- react-side-things/src/components/Cosmetics/Cosmetics.js
+++ react-side-things/src/components/Cosmetics/Cosmetics.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { add } from '../../utilities/storage';
+import { add, multiply, substract } from '../../utilities/storage';
 
 const Cosmetics = () => {
     const first = 55;
     const second = 89;
     const sum = add(first, second);
-    
+    const multi = multiply(first, second);
+    const sub = substract(first, second);
+
     return (
         <div>
 
