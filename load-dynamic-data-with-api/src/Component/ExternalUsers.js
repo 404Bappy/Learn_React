adb1daa29044a04d8bd39b42645a910d1ce0776e
Bappy71 <19103076@iubat.edu>
2023-03-02 01:49:53 +0600

    first commit

--- load-dynamic-data-with-api/src/Component/ExternalUsers.js
+++ load-dynamic-data-with-api/src/Component/ExternalUsers.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
 const ExternalUsers = () => {
-    const [users, setUsers] = useState();
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
-            .then(data => console.log(data))
-    }, [])
+            .then(data => setUsers(data))
+    }, []);
 
     return (
         <div>
             <h3>External Users</h3>
+
+            {
+                users.map(user => <li>{user.name}</li>)
+            }
         </div>
     );
 };
