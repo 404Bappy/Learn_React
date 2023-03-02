631c5adccd445274fe138dfee76b98f469119567
Bappy71 <19103076@iubat.edu>
2023-03-02 11:11:40 +0600

    first commit

--- react-core-recap/src/App.js
+++ react-core-recap/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Count from './Component/Count';
+import LoadComments from './Component/LoadComments';
 
 function App() {
   return (
     <div className="App">
       <Count></Count>
+      <LoadComments></LoadComments>
     </div>
   );
 }
---
+++ react-core-recap/src/Component/LoadComments.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import ShowComment from './ShowComment';
+
+const LoadComments = (props) => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(res => res.json())
+            .then(data => setComments(data))
+    }, [])
+    return (
+        <div>
+            <h3>Usser Comments : </h3>
+            {
+                comments.map(comment => <ShowComment title={comment.title} body={comment.body}  ></ShowComment>)
+            }
+        </div>
+    );
+};
+
+export default LoadComments;
