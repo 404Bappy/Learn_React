d5422cb74387ef793639dfd9098ad958eb9b6e2b
Bappy71 <19103076@iubat.edu>
2023-03-02 10:29:36 +0600

    first commit

--- react-core-recap/src/Component/Count.js
+++ react-core-recap/src/Component/Count.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 
 const Count = () => {
     const [count, setCount] = useState(0);
-    const CountHandler = () => {
+    const CountHandlerIncrease  = () => {
         setCount(count + 1);
+    }
+    
+    const CountHandlerDecrease = () => {
+        setCount(count - 1);
     }
     return (
         <div className='Div_Count'>
             <h1 className='Count'>Count : {count} </h1>
             <h4>Click The Green Button To Increase Count</h4>
-            <button onClick={CountHandler}>INCREASE + </button>
+            <button className='btn' onClick={CountHandlerIncrease}>INCREASE + </button>
+
+            <button className='btn-' onClick={CountHandlerDecrease}>DECREASE - </button>
+
         </div>
     );
 };
